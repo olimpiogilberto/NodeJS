d9961fa20998a79d9a17aff12b212531929a8855
c129427 <olimpiogilberto@gmail.com>
2019-06-16 14:26:47 -0300

    O padrão DAO
    criado uma DAO basica.

--- casadocodigo/src/app/rotas/rotas.js
+++ casadocodigo/src/app/rotas/rotas.js
@@ -1,3 +1,6 @@
+const LivroDao = require('../infra/livro-dao');
+const db = require('../../config/database');
+
 module.exports = (app) =>{
     app.get('/', function(req, resp){
         resp.send(
@@ -18,22 +21,39 @@ module.exports = (app) =>{
     });
 
     app.get('/livros', function(req, resp){
-        resp.marko(
-            require('../views/livros/lista/lista.marko'),
-            {
-                livros: [
-                    {
-                        id: 1,
-                        titulo: 'Fundamentos do Node'
-                    },
-                    {
-                        id: 2,
-                        titulo: 'Node avançado'
-                    }
-                ]
-            }     
-        );
+
+        const livroDao = new LivroDao(db);
+        livroDao.lista(function(erro, resultados){
+            resp.marko(
+                require('../views/livros/lista/lista.marko'),
+                {
+                    livros: resultados
+                }     
+            );
+        })
+        
     });
+
+    // app.get('/livros2', function(req, resp){
+    //     db.all('SELECT * FROM livros', function(erro, resultados){
+    //         resp.marko(
+    //             require('../views/livros/lista/lista.marko'),
+    //             {
+    //                 livros: [
+    //                     {
+    //                         id: 1,
+    //                         titulo: 'Fundamentos do Node'
+    //                     },
+    //                     {
+    //                         id: 2,
+    //                         titulo: 'teste giba'
+    //                     }
+    //                 ]
+    //             }    
+    //         );
+    //     })
+       
+    // });
 };
 
 
